8710153eef554775f4f63f6089b30e5d3a4c22fd
Yuriy B <yberdnikov@me.com>
2013-12-16 22:44:41 +0200

    Added functionality for displaying scores on game screen.

--- SimpleGame/View/GameScene/SGAGameScene.h
+++ SimpleGame/View/GameScene/SGAGameScene.h
@@ -10,4 +10,6 @@
 
 @interface SGAGameScene : SKScene
 
+@property (nonatomic, assign) NSInteger scores;
+
 @end
--- SimpleGame/View/GameScene/SGAGameScene.m
+++ SimpleGame/View/GameScene/SGAGameScene.m
@@ -8,51 +8,71 @@
 
 #import "SGAGameScene.h"
 
+@interface SGAGameScene ()
+@property (nonatomic, strong) SKLabelNode *scoreLabel;
+@end
+
 @implementation SGAGameScene
 
--(id)initWithSize:(CGSize)size
+- (id)initWithSize:(CGSize)size
 {
     if (self = [super initWithSize:size])
     {
         /* Setup your scene here */
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
-        
-        [self addChild:myLabel];
-    }
-    
-    return self;
-}
-
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches)
-    {
-        CGPoint location = [touch locationInNode:self];
+        [self addChild:self.scoreLabel];
         
         SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
+
         SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
         
         [sprite runAction:[SKAction repeatActionForever:action]];
         
         [self addChild:sprite];
     }
+    
+    return self;
 }
 
--(void)update:(CFTimeInterval)currentTime
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    /* Called when a touch begins */
+    
+    for (UITouch *touch in touches)
+    {
+        [self.children enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            SKSpriteNode *sprite = (SKSpriteNode *)obj;
+            sprite.paused = !sprite.paused;
+        }];
+    }
+}
+
+- (void)update:(CFTimeInterval)currentTime
 {
     /* Called before each frame is rendered */
+}
+
+#pragma mark - Properties
+
+- (SKLabelNode *)scoreLabel
+{
+    if (_scoreLabel)
+        return _scoreLabel;
+    
+    _scoreLabel = [SKLabelNode labelNodeWithFontNamed:@"HelveticaNeue"];
+    _scoreLabel.fontSize = 17.0f;
+    _scoreLabel.text = NSLocalizedString(@"Scores", nil);
+    _scoreLabel.position = CGPointMake(CGRectGetWidth(self.scoreLabel.frame) / 2, CGRectGetHeight(self.scoreLabel.frame) / 2);
+    
+    return _scoreLabel;
+}
+
+- (void)setScores:(NSInteger)scores
+{
+    _scores = scores;
+    
+    self.scoreLabel.text = [[NSString alloc] initWithFormat:@"%@: %d", NSLocalizedString(@"Scores", nil), scores];
 }
 
 @end
